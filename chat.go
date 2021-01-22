1579def23c1b9c1e35456ae48a50d971f1405e52
J0Miles <jordanamiles@hotmail.com>
2021-01-21 20:30:27 -0600

    Added terminal print to specified user color

--- chat.go
+++ chat.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strconv"
 	"context"
 	"encoding/json"
 	"net/http"
@@ -9,15 +10,30 @@ import (
 	"strings"
 	"time"
 	"bnsvc.net/tmi/ircon"
+	"github.com/gookit/color"
 )
+
+type Hex string
+
+type RGB struct {
+	Red   uint8
+	Green uint8
+	Blue  uint8
+}
 
 type handler struct {
 	Con *ircon.IRCon
 	Channels []string
 }
 
+type MsgObj struct {
+	Color string `json:"color"`
+	RoomID string `json:"room-id"`
+	Subscriber string `json:"subscriber"`
+}
+
 func (h *handler) Connected() {
-	fmt.Println("#", time.Now(), "Connected")
+	fmt.Println("->", time.Now(), "Connected")
 	go func() {
 		N := 50
 		for i := 0; i < len(h.Channels); i += N {
@@ -36,10 +52,53 @@ func (handler) Disconnected(err error) {
 	fmt.Println("#", time.Now(), "Disconnected:", err)
 }
 
-func (handler) Message(msg *ircon.Message) {
-//	fmt.Println(msg.Raw())
-	fmt.Println(msg.Args)
+
+func ParseHex(class string) (color.RGBColor) {
+	var rgb RGB                              
+	var err error
+	s := strings.Split(class, ";")
+	for i := range s {
+		cs := strings.Contains(s[i], "color=")
+		if cs {
+		 hexValue := strings.Split(s[i], "=")
+		 var removeHash = strings.Split(hexValue[1], "#")
+		 justHex := strings.Join(removeHash, "")
+		 var hex Hex = Hex(justHex)
+		 rgb, err = Hex2RGB(hex)
+		 if err != nil {
+		panic("Couldn't convert hex to rgb")
+	}
+	 }
 }
+c := color.RGB(rgb.Red, rgb.Green, rgb.Blue)
+return c
+}
+
+func (h Hex) toRGB() (RGB, error) {
+	return Hex2RGB(h)
+}
+
+func Hex2RGB(hex Hex) (RGB, error) {
+	var rgb RGB
+	values, err := strconv.ParseUint(string(hex), 16, 32)
+	if err != nil {
+		return RGB{}, err
+	}
+
+	rgb = RGB{
+		Red:   uint8(values >> 16),
+		Green: uint8((values >> 8) & 0xFF),
+		Blue:  uint8(values & 0xFF),
+	}
+
+	return rgb, nil
+}
+
+func (handler) Message(msg *ircon.Message) {
+	rgbColor := ParseHex(msg.Raw())
+	rgbColor.Println(msg.Args)
+}
+
 
 func main() {
 	username := os.Getenv("TWITCH_USERNAME")
